406fab735c65e8be3eddea83fb0eef3d427d9d0c
ivanfigueiredo <77892424+ivanfigueiredo@users.noreply.github.com>
2021-11-29 20:57:12 -0300

    Update EstoqueController.js

--- src/controllers/EstoqueController.js
+++ src/controllers/EstoqueController.js
@@ -72,7 +72,7 @@ module.exports = {
 
     listarEstolMobile: async (req, res) => {
         let produto = await Produto.find({status: "Ativo"});
-        let estoque = await Estoque.find({vencido: true});
+        let estoque = await Estoque.find({vencido: false});
 
         let listaEstoque = [];
 
